085d027ea174c825bc94cf95a1bcba58e9adeaa3
agvolodin <agvolodin@stdu.info>
2025-05-08 21:43:49 +0400

    добавил расчёт параметров DC-DC конвертора на MC34063

---
+++ hw11/34063.py
@@ -0,0 +1,43 @@
+from env import Vsat, U_frwd
+from argparse import ArgumentParser
+
+
+def step_up_l_calculate(Freq: float, Vin: float, Vout: float, Iout: float):
+    T = 1/Freq
+    t_ratio = (Vout+U_frwd-Vin)/(Vin-Vsat)
+    t_off = T/(t_ratio + 1)
+    t_on = T-t_off
+    C_t = 4*0.00001*t_on
+    i_pk = 2*Iout*(t_ratio+1)
+    L = ((Vin-Vsat)/i_pk)*t_on
+    print(
+        f"Freq = {Freq/1000} kHz  C = {(C_t*1e12):.2f} pF  L >= {(L*1000000):.2f} uH")
+
+
+def step_down_l_calculate(Freq: float, Vin: float, Vout: float, Iout: float):
+    T = 1/Freq
+    t_ratio = (Vout+U_frwd)/(Vin-Vsat-Vout)
+    t_off = T/(t_ratio + 1)
+    t_on = T-t_off
+    C_t = 4*0.00001*t_on
+    i_pk = 2*Iout
+    L = ((Vin-Vsat-Vout)/i_pk)*t_on
+    print(
+        f"Freq = {Freq/1000} kHz  C = {(C_t*1e12):.2f} pF  L >= {(L*1000000):.2f} uH")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="Расчёт парметров DC-DC конветора на ИМС MC34063")
+    parser.add_argument('v_in', type=float, help="входное напряжение, В")
+    parser.add_argument('v_out', type=float, help="выходное напряжение, В")
+    parser.add_argument('i_out', type=float, help="выходной ток, мА")
+    args = parser.parse_args()
+    Vin = args.v_in
+    Vout = args.v_out
+    Iout = args.i_out/1000
+    for fr in range(10, 100, 10):
+        if (Vout >= Vin):
+            step_up_l_calculate(Freq=fr*1000, Vin=Vin, Vout=Vout, Iout=Iout)
+        else:
+            step_down_l_calculate(Freq=fr*1000, Vin=Vin, Vout=Vout, Iout=Iout)
